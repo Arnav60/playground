f7a215edc1e519289b17cc325c054a688985c682
Arnav S Awasthi <arnavawasthy60@gmail.com>
2018-04-27 22:05:31 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+/*const numbers = [1, 2, 3, 4, 5, 6]
+console.log(numbers)
+numbers.pop(6)
+console.log(numbers)
+console.log(typeof numbers)*/
+/*let val;
+val = (374).toString;
+console.log(val)
+val = parseFloat('2988');
+console.log(val)
+console.log(typeof val)*/
+const val1 = '5';
+const val2 = 6;
+const val = parseInt(val1) + val2;
+console.log(val)
+console.log(typeof val)
+
+
